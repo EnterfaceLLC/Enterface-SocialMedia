11da97c497753212d0c8dbc8024e80c4248ee0b3
Rogelio Zamora Jr <dotenterface@gmail.com>
2023-01-29 11:09:27 -0600

    Installed expo packages/modules

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import HomeScreen from './src/screens/HomeScreen/HomeScreen';
 import CommentScreen from './src/screens/CommentScreen';
 import ProfileScreen from './src/screens/ProfileScreen/ProfileScreen';
 import EditProfileScreen from './src/screens/EditProfileScreen';
+import PostUploadScreen from './src/screens/PostUploadScreen';
 
 
 const App = () => {
   return (
     <View style={styles.app}>
-      <EditProfileScreen />
+      <PostUploadScreen />
     </View>
   );
 };
---
+++ src/screens/PostUploadScreen/styles.ts
@@ -0,0 +1,6 @@
+import { StyleSheet } from "react-native";
+import fonts from "../../theme/fonts";
+import colors from "../../theme/colors";
+
+
+export default StyleSheet.create({});
