12130d96b2da1d51b48f150e4f147ef93bfa38e3
Rogelio Zamora Jr <dotenterface@gmail.com>
2023-02-01 22:39:14 -0600

    PostUploadScreen Added; initial camera view set-up; module 3.14

--- src/screens/PostUploadScreen/PostUploadScreen.tsx
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -1,11 +1,32 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './styles';
+import { Camera } from 'expo-camera';
 
 const PostUploadScreen = () => {
+  const [permission, setPermission] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const getPermission = async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const microphonePermission = await Camera.requestMicrophonePermissionsAsync();
+      setPermission(cameraPermission.status === 'granted' && microphonePermission.status === 'granted');
+    };
+    getPermission();
+  }, [])
+
+  if (permission === null) {
+    return <Text>LOADING...</Text>
+  }
+
+  if (permission === false) {
+    return <Text>ACCESS DENIED</Text>
+  }
+
+
   return (
-    <View>
-      <Text>PostUploadScreen</Text>
+    <View style={styles.page}>
+      <Camera style={styles.camera} />
     </View>
   )
 }
--- src/screens/PostUploadScreen/styles.ts
+++ src/screens/PostUploadScreen/styles.ts
@@ -3,4 +3,14 @@ import fonts from "../../theme/fonts";
 import colors from "../../theme/colors";
 
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: colors.primary,
+  },
+  camera: {
+    width: '100%',
+    aspectRatio: 3 / 4,
+  },
+});
