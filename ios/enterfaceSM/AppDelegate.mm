86a8046631397071b576d5c1051de717dac65fce
Rogelio Zamora Jr <dotenterface@gmail.com>
2023-02-22 09:57:58 -0600

    +IOS Deep Linking & Config

--- ios/enterfaceSM/AppDelegate.mm
+++ ios/enterfaceSM/AppDelegate.mm
@@ -14,6 +14,8 @@
 #import <React/RCTSurfacePresenterBridgeAdapter.h>
 #import <ReactCommon/RCTTurboModuleManager.h>
 
+#import <React/RCTLinkingManager.h>
+
 #import <react/config/ReactNativeConfig.h>
 
 static NSString *const kRNConcurrentRoot = @"concurrentRoot";
@@ -130,5 +132,12 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 }
 
 #endif
+
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
 
 @end
